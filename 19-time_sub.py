fa4dcc01eb6680b124f9186209df691e62ec0a29
SoleSensei <dimargfd@gmail.com>
2018-02-21 00:49:52 +0300

    19

---
+++ 19-time_sub.py
@@ -0,0 +1,9 @@
+h1 = int(input())
+m1 = int(input())
+s1 = int(input())
+h2 = int(input())
+m2 = int(input())
+s2 = int(input())
+time1 = h1*60**2 + m1*60 + s1
+time2 = h2*60**2 + m2*60 + s2
+print(time2 - time1)
