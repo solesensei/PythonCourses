8bbba13bd5191ed379b75765eabe25f17fa72c13
SoleSensei <dimargfd@gmail.com>
2018-11-04 19:07:52 +0300

    words-shuffle updated

--- MSU Python 2018/Task5/4-words-shuffle.py
+++ MSU Python 2018/Task5/4-words-shuffle.py
@@ -1,10 +1,13 @@
 # Check shuffle of words coincidence
 
+
 s1 = input().upper()
 s2 = input().upper()
 
-words_input = ''.join((char if char.isalnum() else ' ') for char in s1).split()
-words_shuffle = ''.join((char if char.isalnum() else ' ') for char in s2).split()
+words_input = [word if word[-1].isalnum() else word[:-1] for word in s1.split()]
+words_shuffle = [word if word[-1].isalnum() else word[:-1] for word in s2.split()]
+words_input += [word[-1] if not word[-1].isalnum() else '' for word in s1.split()] 
+words_shuffle += [word[-1] if not word[-1].isalnum() else '' for word in s2.split()]
 
 for word in words_input:
     if word in words_shuffle:
