92584170e65d0f3bd7f83077f385816f9f676725
SoleSensei <dimargfd@gmail.com>
2018-11-04 12:28:07 +0300

    MSU/Task 4/3 Updated

--- MSU Python 2018/Task4/3-look-say.py
+++ MSU Python 2018/Task4/3-look-say.py
@@ -1,25 +1,16 @@
 # Look Say generator
+from itertools import groupby
+
 
 def LookSay():
-    ps = '1'
-    yield ps
-    while 1:
-        s = ''
-        d = list(ps)[0]
-        k = 0
-        for i in list(ps):
-            if d != i:
-                s += str(k) + str(d)
-                d = i
-                k = 0
-            k += 1
-        s += str(k) + str(d)
-        ps = s
-        for i in list(s):
+    s = '1'
+    yield s
+    while True:
+        s = ''.join(str(len(list(group))) + key for key, group in groupby(s))
+        for i in s:
             yield i
 
-
-# for i,l in enumerate(LookSay()):
-#     print(f"{i}: {l}")
-#     if i>10:
-#         break
+# for i, l in enumerate(LookSay()):
+    # print(f"{i}: {l}")
+    # if i > 15:
+    #     break
