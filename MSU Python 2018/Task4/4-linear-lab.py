a0b92a92af810f70a6b7ae8db2ac220d61ade763
SoleSensei <dimargfd@gmail.com>
2018-11-04 19:20:06 +0300

    linear-lab updating

--- MSU Python 2018/Task4/4-linear-lab.py
+++ MSU Python 2018/Task4/4-linear-lab.py
@@ -1,15 +1,15 @@
-# Linear labyrinth: go from [0] to [-1]  
+# Linear labyrinth: go from [0] to [-1]
 
 def run_labyrinth(k, A, was_here):
-    if k >= len(A) or k < 0 or was_here[k] == 1:
+    if k >= len(A) or k < 0 or k in was_here:
         return False
-    was_here[k] = 1
-    if k == -1 or k == len(A) - 1:
+    was_here.add(k)
+    if k == len(A) - 1:
         return True
     return run_labyrinth(k+A[k], A, was_here) or run_labyrinth(k-1, A, was_here)
 
 A = eval(input())
-was_here = [0 for i in A]
+was_here = set()
 passed = run_labyrinth(0, A, was_here)
 if passed:
     print('YES')
