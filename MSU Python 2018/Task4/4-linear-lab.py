e1b9d429f3f30779c0fde58599f39a8e26f5da55
SoleSensei <dimargfd@gmail.com>
2018-11-08 20:19:55 +0300

    tasks update

--- MSU Python 2018/Task4/4-linear-lab.py
+++ MSU Python 2018/Task4/4-linear-lab.py
@@ -1,17 +1,30 @@
 # Linear labyrinth: go from [0] to [-1]
 
-def run_labyrinth(k, A, was_here):
-    if k >= len(A) or k < 0 or k in was_here:
-        return False
-    was_here.add(k)
-    if k == len(A) - 1:
-        return True
-    return run_labyrinth(k+A[k], A, was_here) or run_labyrinth(k-1, A, was_here)
+def run_labyrinth(A, was_here):
+    part = A[0] if A[0] < len(A) else -1
+    k = 0
+    while k <= part:
+        if A[k] + k == len(A) - 1:
+            return True
+        if part < A[k] + k and A[k] + k < len(A):
+            part = A[k] + k
+        k += 1
+    return False
 
 A = eval(input())
 was_here = set()
-passed = run_labyrinth(0, A, was_here)
+passed = run_labyrinth(A, was_here)
 if passed:
     print('YES')
 else:
     print('NO')
+
+
+# recursion
+# def run_labyrinth_rec(k, A, was_here):
+#     if k >= len(A) or k < 0 or k in was_here:
+#         return False
+#     was_here.add(k)
+#     if k == len(A) - 1:
+#         return True
+#     return run_labyrinth(k+A[k], A, was_here) or run_labyrinth(k-1, A, was_here)
--- MSU Python 2018/Task5/2-pattern-search.py
+++ MSU Python 2018/Task5/2-pattern-search.py
@@ -4,27 +4,29 @@ s = str(input())
 pattern = str(input())
 
 pat = pattern.split('@')
-
 found = False
+idx = 0
+absolute_idx = 0
 if len(pat)-1 <= len(s):
-    while s and not found:
+    while s and not found and idx != -1:
         offset = 0
         found = True
         for i, part in enumerate(pat):
             if i == 0:
                 idx = a = s.find(part)
             else:
-                a = 1 if s[offset:offset + len(part)] == part else -1
-            if a != -1 and len(part) != 0:
+                a = 0 if s[offset:offset + len(part)] == part else -1
+            if a != -1:
                 offset += len(part) + a + 1
             else:
                 found = False
                 break
+        if found and idx == 0:
+            break
+        absolute_idx += 1 if idx <= 0 else idx
         s = s[1 if idx <= 0 else idx:]
 
-
-
 if found:
-    print(idx)
+    print(absolute_idx)
 else:
     print(-1)
